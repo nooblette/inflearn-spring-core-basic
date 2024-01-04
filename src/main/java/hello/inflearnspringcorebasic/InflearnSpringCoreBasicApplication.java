498dc45b269d0194c8623c3fb1e86da33ac4bb08
nooblette <min9hyuk@gmail.com>
2024-01-04 21:38:26 +0900

    feat : 프로젝트 초기 설정

---
+++ src/main/java/hello/inflearnspringcorebasic/InflearnSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.inflearnspringcorebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringCoreBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringCoreBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/inflearnspringcorebasic/InflearnSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.inflearnspringcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
