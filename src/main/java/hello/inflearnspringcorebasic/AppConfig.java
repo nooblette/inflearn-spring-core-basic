ab9bb516c26f825642b25abe2bbae0bc31fc3edf
nooblette <min9hyuk@gmail.com>
2024-01-06 14:38:06 +0900

    feat : sec 03. 스프링 핵심 원리 이해2 - AppConfig 리팩터링

--- src/main/java/hello/inflearnspringcorebasic/AppConfig.java
+++ src/main/java/hello/inflearnspringcorebasic/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.inflearnspringcorebasic;
 
+import hello.inflearnspringcorebasic.discount.DiscountPolicy;
 import hello.inflearnspringcorebasic.discount.FixDiscountPolicy;
+import hello.inflearnspringcorebasic.member.MemberRepository;
 import hello.inflearnspringcorebasic.member.MemberService;
 import hello.inflearnspringcorebasic.member.MemberServiceImpl;
 import hello.inflearnspringcorebasic.member.MemoryMemberRepository;
@@ -11,6 +13,7 @@ import hello.inflearnspringcorebasic.order.OrderServiceImpl;
 * 애플리케이션(App)의 전체 동작 방식을 구성(Config)
 * "구현 객체를 생성"하고 "연결"하는 책임을 갖는 별도의 설정 클래스
 */
+// 현재 AppConfig 클래스는 어떤 객체가 어떤 객체들과 의존하고 있는지가 간단하게 한눈에 보이지 않으며, 코드 중복도 많이 존재한다.
 public class AppConfig {
 	/**
 	 * 애플리케이션의 실제 동작에 필요한 구현 객체를 생성 한다.
@@ -21,7 +24,7 @@ public class AppConfig {
 		 * 객체 인스턴스의 참조(레퍼런스)를 생성자 주입(Injection) (= 생성자를 통해 구현 객체를 연결)
 		 * MemverServiceImpl -> MemoryMemberRepository
 		 */
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(createMemberRepository());
 	}
 
 	public OrderService orderService(){
@@ -29,6 +32,18 @@ public class AppConfig {
 		 * 객체 인스턴스의 참조(레퍼런스)를 생성자 주입 (= 생성자를 통해 구현 객체를 연결)
 		 * OrderServiceImpl -> MemoryMemberRepository, FixDiscountPolicy
 		 * */
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(createMemberRepository(), createDisCountPolicy());
+	}
+
+	// command + option + m : 중복 코드 메서드 추출(리팩토링)
+	// 메서드 추출로 현재 MemberRepository 역할은 MemoryMemberRepository 구현 클래스가 수행한다는 것을 명확히 한눈에 드러낸다.
+	// MemberRepository 인터페이스의 구현 객체가 변경되더라도 createMemberRepository() 메서드만 수정하여 영향 범위를 줄일 수 있다.
+	private MemberRepository createMemberRepository() {
+		return new MemoryMemberRepository();
+	}
+
+	// 메서드 추출로 현재 DiscountPolicy 역할은 FixDiscountPolicy 구현 클래스가 수행한다는 것을 명확히 한눈에 드러낸다.
+	private DiscountPolicy createDisCountPolicy(){
+		return new FixDiscountPolicy();
 	}
 }
