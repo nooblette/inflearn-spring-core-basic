2ac1602829b68f4f137c9b034922253cb33034b7
nooblette <min9hyuk@gmail.com>
2024-01-06 13:34:57 +0900

    feat : sec 03. 스프링 핵심 원리 이해2 - 새로운 할인 정책 적용과 문제점

--- src/main/java/hello/inflearnspringcorebasic/order/OrderServiceImpl.java
+++ src/main/java/hello/inflearnspringcorebasic/order/OrderServiceImpl.java
@@ -1,14 +1,22 @@
 package hello.inflearnspringcorebasic.order;
 
 import hello.inflearnspringcorebasic.discount.DiscountPolicy;
-import hello.inflearnspringcorebasic.discount.FixDiscountPolicy;
 import hello.inflearnspringcorebasic.member.Member;
 import hello.inflearnspringcorebasic.member.MemberRepository;
 import hello.inflearnspringcorebasic.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 	private final MemberRepository memberRepository = new MemoryMemberRepository();
-	private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+	// 클라이언트인 OrderServiceImpl 클래스는 DiscountPolicy 인터페이스와 FixDiscountPolicy(구현 객체) 클래스에 직접적으로 의존 (DIP 위반)
+	// 할인 정책을 변경하려면 클라이언트의 코드를 수정해야한다. (OCP 위반)
+	// private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+	/**
+	 * 	DIP를 준수하기 위해 인터페이스에만 의존하도록 코드를 변경
+	 * 	하지만 누군가가 클라이언트인 OrderServiceImpl 클래스에 discountPolicy 인터페이스의 구현 객체를 대신 생성하고 넣어주어야 한다.
+ 	 */
+	private DiscountPolicy discountPolicy;
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
