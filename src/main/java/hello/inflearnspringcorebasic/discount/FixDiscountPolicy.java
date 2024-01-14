46fba77730e3500cb9e24b304ddcd573e746678c
nooblette <min9hyuk@gmail.com>
2024-01-14 21:54:16 +0900

    feat : sec 07. 의존관계 자동 주입 - @Primary

--- src/main/java/hello/inflearnspringcorebasic/discount/FixDiscountPolicy.java
+++ src/main/java/hello/inflearnspringcorebasic/discount/FixDiscountPolicy.java
@@ -7,7 +7,6 @@ import hello.inflearnspringcorebasic.member.Grade;
 import hello.inflearnspringcorebasic.member.Member;
 
 @Component
-@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 	private int discountFixAmount = 1000; // 1000원 할인
 
--- src/main/java/hello/inflearnspringcorebasic/discount/RateDiscountPolicy.java
+++ src/main/java/hello/inflearnspringcorebasic/discount/RateDiscountPolicy.java
@@ -1,13 +1,14 @@
 package hello.inflearnspringcorebasic.discount;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import hello.inflearnspringcorebasic.member.Grade;
 import hello.inflearnspringcorebasic.member.Member;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 	private int discountPercent = 10;
 	@Override
