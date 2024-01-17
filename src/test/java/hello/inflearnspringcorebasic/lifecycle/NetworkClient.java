ac079a28e6e82cf0e2490395cb99ec9a3e15830d
nooblette <min9hyuk@gmail.com>
2024-01-17 18:12:08 +0900

    feat : sec 08. 빈 생명주기 콜백 - 인터페이스 InitializingBean, DisposableBean

--- src/test/java/hello/inflearnspringcorebasic/lifecycle/NetworkClient.java
+++ src/test/java/hello/inflearnspringcorebasic/lifecycle/NetworkClient.java
@@ -1,8 +1,9 @@
 package hello.inflearnspringcorebasic.lifecycle;
 
-import net.minidev.json.JSONUtil;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean {
 	private String url;
 
 	public NetworkClient() {
@@ -16,18 +17,30 @@ public class NetworkClient {
 	}
 
 	// 서비스 시작시 호출
-	public void connect(){
+	public void connect() {
 		System.out.println("connect to " + url);
 	}
 
 	// 네트워크 연결이 된 상태에서 호출
-	public void call(String message){
+	public void call(String message) {
 		System.out.println("call to " + url + ", message = " + message);
 	}
 
 	// 서비스 종료시 호출
-	public void disconnect(){
+	public void disconnect() {
 		System.out.println("close");
 	}
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("\n빈 생성 및 의존관계 주입 후 NetworkClient.afterPropertiesSet() 호출");
+		connect();
+		call("초기화 연결 메시지");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("\n스프링 종료 및 빈 소멸 전 NetworkClient.destroy() 호출");
+		disconnect();
+	}
 }
