b12968b9844cafbe8a10106434a8d6ccdbff9583
nooblette <min9hyuk@gmail.com>
2024-01-19 21:55:59 +0900

    feat : sec 09. 빈 스코프 - 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점(싱글톤에서 프로토타입 빈 사용)

--- src/test/java/hello/inflearnspringcorebasic/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/inflearnspringcorebasic/scope/SingletonWithPrototypeTest1.java
@@ -2,9 +2,9 @@ package hello.inflearnspringcorebasic.scope;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -26,6 +26,36 @@ public class SingletonWithPrototypeTest1 {
 		PrototypeBean prototypeBean2 = applicationContext.getBean(PrototypeBean.class);
 		prototypeBean2.addCount();
 		assertThat(prototypeBean2.getCount()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("싱글톤에서 프로토타입 빈을 사용하는 경우 프로토타입 스코프의 빈을 사용할때마다 새로 생성되는지 count 필드 값으로 테스트 한다.")
+	void singleTonClientUsePrototype(){
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+			ClientBean.class, PrototypeBean.class); // 두개의 스프링 구성설정 클래스를 AnnotationConfigApplicationContext 대상으로 등록
+		ClientBean clientBean1 = applicationContext.getBean(ClientBean.class);
+		int count1 = clientBean1.logic();
+		assertThat(count1).isEqualTo(1);
+
+		ClientBean clientBean2 = applicationContext.getBean(ClientBean.class);
+		int count2 = clientBean2.logic();
+		assertThat(count2).isEqualTo(2);
+
+	}
+
+	@Scope("singleton") // 생략 가능
+	static class ClientBean {
+		private final PrototypeBean prototypeBean;
+
+		@Autowired // 생성자가 1개면 @Autowired 어노테이션은 생략 가능
+		public ClientBean(PrototypeBean prototypeBean){
+			this.prototypeBean = prototypeBean;
+		}
+
+		public int logic(){
+			prototypeBean.addCount();
+			return prototypeBean.getCount();
+		}
 	}
 
 	@Scope("prototype")
