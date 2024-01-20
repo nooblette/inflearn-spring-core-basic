9d7d1cce67f2bba08159618a2b0231aba7db36ea
nooblette <min9hyuk@gmail.com>
2024-01-20 16:39:24 +0900

    feat : sec 09. 빈 스코프 - 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제해결 - JSR-330 Provider

--- src/test/java/hello/inflearnspringcorebasic/scope/PrototypeProviderTest.java
+++ src/test/java/hello/inflearnspringcorebasic/scope/PrototypeProviderTest.java
@@ -2,10 +2,10 @@ package hello.inflearnspringcorebasic.scope;
 
 import static org.assertj.core.api.Assertions.*;
 
+import javax.inject.Provider;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -15,7 +15,7 @@ import jakarta.annotation.PreDestroy;
 public class PrototypeProviderTest {
 
 	@Test
-	@DisplayName("싱글톤 빈에서 프로토타입 빈을 사용하는 경우 새로운 프로토타입 빈을 생성하기 위해 ObjectProvider 클래스에  요청한다.")
+	@DisplayName("싱글톤 빈에서 프로토타입 빈을 사용하는 경우 새로운 프로토타입 빈을 생성하기 위해 javax.inject 패키지의 provider 라이브러리를 사용한다.")
 	void providerTest(){
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
 			PrototypeBean.class, ClientBean.class);
@@ -31,12 +31,11 @@ public class PrototypeProviderTest {
 
 	@Scope("singleton") // 생략 가능
 	static class ClientBean {
-		@Autowired
-		private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+		private Provider<PrototypeBean> prototypeBeanProvider;
 
 		public int logic(){
 			// ObjectProvider Wrapper 클래스가 대신 프로토타입 스코프 빈에 대한 의존관계를 찾아(DL, Dependency Lookup) 준다.
-			PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+			PrototypeBean prototypeBean = prototypeBeanProvider.get();
 			System.out.println("prototypeBean = " + prototypeBean); // logic() 메서드 호출마다 다른 인스턴스 참조값 츌력
 			prototypeBean.addCount();
 			return prototypeBean.getCount();
